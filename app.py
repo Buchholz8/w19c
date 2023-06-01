80c1a48edeb6e527ea412a8b5c880d38d38f07fa
markb <markbuchholz8@gmail.com>
2023-05-31 23:14:07 -0600

    tried fixing looping issue

--- app.py
+++ app.py
@@ -3,14 +3,20 @@ import random
 
 user_health = 100
 computer_health = 100
-attack_input = user_move
-defense_input = user_defence
-computer_attack = 'punch' if random.randint(0, 100) < 50 else 'kick'
-computer_defense = 'high' if random.randint(0, 100) < 50 else 'low'
+
 while user_health > 0 and computer_health > 0:
     print('User Health:', user_health)
     print('Computer Health:', computer_health)
     print()
+
+    attack_input = user_move
+    defense_input = user_defence
+
+    if attack_input is None or defense_input is None:
+        continue
+
+    computer_attack = 'punch' if random.randint(0, 100) < 50 else 'kick'
+    computer_defense = 'high' if random.randint(0, 100) < 50 else 'low'
 
     if attack_input == 1 and computer_defense == 'low':
         print('User hits the computer!')
@@ -18,10 +24,10 @@ while user_health > 0 and computer_health > 0:
     elif attack_input == 2 and computer_defense == 'high':
         print('User hits the computer!')
         computer_health -= 10
-    elif computer_attack == "punch" and defense_input == 2:
+    elif computer_attack == 'punch' and defense_input == 2:
         print('Computer hits the user!')
         user_health -= 10
-    elif computer_attack == "kick" and defense_input == 1:
+    elif computer_attack == 'kick' and defense_input == 1:
         print('Computer hits the user!')
         user_health -= 10
 
